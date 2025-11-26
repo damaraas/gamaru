ca7039e787a83ef19f77837e5722e551f377395b
I Putu Sukma Widyantara <68043141+sukmaputu@users.noreply.github.com>
2025-11-25 21:24:13 -0800

    revisi whatsapp program-teens

--- resources/js/Pages/Little/ProgramTeens.vue
+++ resources/js/Pages/Little/ProgramTeens.vue
@@ -137,7 +137,7 @@
 
                     <!-- CTA Button -->
                     <a
-                        href="https://wa.me/6285712230349?text=Halo MinBi! Saya mau tanya info mengenai les privat di GABI. Bisa dibantu?"
+                        href="https://wa.me/6285712230349?text=Halo MinXue! Saya mau tanya info mengenai les privat Mandarin di GAMARU. Bisa dibantu?"
                         target="_blank"
                         class="items-center gap-3 bg-green text-white font-semibold text-lg md:text-xl px-8 py-4 rounded-xl transition-all duration-300 transform hover:scale-105 shadow-lg hover:shadow-xl lg:inline-flex hidden"
                     >
@@ -162,7 +162,7 @@
                 </div>
 
                 <a
-                    href="https://wa.me/6285712230349?text=Halo MinBi! Saya mau tanya info mengenai les privat di GABI. Bisa dibantu?"
+                    href="https://wa.me/6285712230349?text=Halo MinXue! Saya mau tanya info mengenai les privat Mandarin di GAMARU. Bisa dibantu?"
                     target="_blank"
                     class="items-center gap-3 bg-green text-white font-semibold text-lg md:text-xl px-6 py-4 rounded-2xl transition-all duration-300 transform hover:scale-105 shadow-lg hover:shadow-xl lg:hidden inline-flex mt-10"
                 >
